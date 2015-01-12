ee04a7fac669d6a6fd39a68b9f2b570f03db388e
summer <summer_z1993@sina.com>
2015-01-12 12:51:43 +0800

    [zy]首页部分内容

--- src/com/zy/tofree/TabActivityHome.java
+++ src/com/zy/tofree/TabActivityHome.java
@@ -1,7 +1,14 @@
 package com.zy.tofree;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class TabActivityHome extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
 
 }
